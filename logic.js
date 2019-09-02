a1c1b3c782ca1f4a7d73f38980ebcff9e0fc559e
Kevin Spies <kevin.spies@gmail.com>
2019-09-02 12:48:46 -0400

    concat was the issue

--- logic.js
+++ logic.js
@@ -16,27 +16,22 @@ let wins = 0;
 let losses = 0;
 let gameIsOver = false;
 
-//let's do this with a string, grabbing the word, and an array, to keep track of their progress
 let progressArr = [];
 for (let i = 0; i < word.length; i++) {
   progressArr.push("_");
-  //start by filling their progressArray with underscores
 }
-//function to update a correct guess into progressArr
 function update(letter) {
-  //letter represents user guess
   for (let i = 0; i < word.length; i++) {
     if (word.charAt(i) == letter) {
       progressArr[i] = letter;
     }
   }
+  document.querySelector("#currentWord").innerHTML = showProgress(progressArr);
 }
-//this function builds a new string to show the user every time they make a corret guess, adding a space
-//after each of the elements in their current progress array
-function showProgress(progressArr) {
+function showProgress(someArr) {
   let progressString = "";
-  for (let i = 0; i < progressArr.length; i++) {
-    progressString.concat(progressArr[i], " ");
+  for (let i = 0; i < someArr.length; i++) {
+    progressString+=someArr[i]+" ";
   }
   return progressString;
 }
@@ -46,7 +41,6 @@ let lettersGuessed = [];
 let guess = "";
 
 console.log(progressArr);
-//game logic here?
 document.onkeyup = function(event) {
     guess = event.key.toLowerCase();
     if(word.includes(guess)){
@@ -57,24 +51,20 @@ document.onkeyup = function(event) {
 
 };
 
-//useful things..
+// useful things..
 // let newWord = words[Math.floor(Math.random() * words.length)];
 // document.querySelector("#currentWord").innerHTML = newWordHidden;
 // document.onkeyup = function(event) {
 // var guess = event.key.toLowerCase();
 
-{
-  /* 
-<p>Wins: <span id="numWins">0</span></p>
-<p>Losses: <span id="numLosses">0</span></p>
-<p>Current Word: <span id="currentWord"></span></p>
-<p>Number of guesses remaining: <span id="numGuessesRemaining"></span></p>
-<p>Letters already guessed: <span id="lettersAlreadyGuessed"></span></p> 
-*/
-}
+// <p>Wins: <span id="numWins">0</span></p>
+// <p>Losses: <span id="numLosses">0</span></p>
+// <p>Current Word: <span id="currentWord"></span></p>
+// <p>Number of guesses remaining: <span id="numGuessesRemaining"></span></p>
+// <p>Letters already guessed: <span id="lettersAlreadyGuessed"></span></p> 
 
-//ideas i could implement -
-//webscraping new libraries (user option to mix things up)
-//cool css animations (like green fade effect on corrent letter, red on incorrect for example)
-//sounds
-//animations
+// ideas i could implement -
+// webscraping new libraries (user option to mix things up)
+// cool css animations (like green fade effect on corrent letter, red on incorrect for example)
+// sounds
+// animations
